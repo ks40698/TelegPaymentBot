ce4c9c16bede7992c8437ace18032420c07f6c2e
ks40698 <77614375+ks40698@users.noreply.github.com>
2021-08-13 17:30:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 """ Data input for bot """
 
-BOT_TOKEN = '1790423596:AAFbieU3yo2xwHEjwLosI_WWVEpAtR-nrKI'
-PROVIDER_TOKEN ='284685063:TEST:NTcxMWZlZDJkMGUw'
-CURRENCY = 'USD'
-INVOICE_TITLE = "MerchantPay"
+BOT_TOKEN = '1929795899:AAFr3oN9oDHh8vitZFXhrd-x7FyldRQMy0c'
+PROVIDER_TOKEN '284685063:TEST:ZGRmYmQzMTdkNTc2'
+CURRENCY = 'INR'
+INVOICE_TITLE = "DOOO INDIA"
 INVOICE_DESCRIPTION = "Use this invoice to make payment"
 START_MESSAGE = "Use /createinvoice to create an invoice. \nUse /help to read info."
-CREATE_INVOICE_MESSAGE = f"Please enter an amount (in {CURRENCY}).\n (Limit is $10,000.\n Decimal places greater than 2 will be rounded up)."
+CREATE_INVOICE_MESSAGE = f"Please enter an amount (in {CURRENCY}).\n (Limit is ₹10,000.\n Decimal places greater than 2 will be rounded up)."
 AMOUNT_INVALID_MESSAGE = "Please enter a valid amount."
 AMOUNT_TOO_BIG_MESSAGE = "Amount exceeds limit.\nEnter a valid amount."
 CANCEL_MESSAGE = "cancelled operation"
